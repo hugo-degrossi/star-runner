95f67bf62ec079d8bf5298b4786fed28036bf34a
Hugo De Grossi (TME) <hugo.de.grossi@toyota-europe.com>
2021-12-12 22:27:49 +0100

    Added Join Button and animation before contestor page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './static/scss/main.scss';
 
+
 function App() {
+
+  // state
+  const [isJoinClicked, setIsJoinClicked] = useState<Boolean>(false)
+
+  // handlers
+  const handleJoinClick = () => {setIsJoinClicked(true)}
+
   return (
     <div className="App">
-      <svg viewBox="0 0 600 300">
-        <symbol id="s-text">
-          <text text-anchor="middle" x="50%" y="50%" dy=".35em">STAR RUNNER</text>
-        </symbol>
-        <use className="title-text" xlinkHref="#s-text"></use>
-        <use className="title-text" xlinkHref="#s-text"></use>
-      </svg>
+    
+      <div className={isJoinClicked ? "launcher" + ' ' + 'launcher-top' : 'launcher'}>
+        <svg viewBox="0 0 600 100">
+          <symbol id="s-text">
+            <text text-anchor="middle" x="50%" y="50%" dy=".35em">STAR RUNNER</text>
+          </symbol>
+          <use className="title-text" xlinkHref="#s-text"></use>
+          <use className="title-text" xlinkHref="#s-text"></use>
+        </svg>
 
+
+        
+
+
+      </div>
+      {isJoinClicked
+          ? null
+          : <div className='btn' onClick={handleJoinClick}>
+          JOIN THE ADVENTURE
+        </div>
+        }
 
     </div>
   );
